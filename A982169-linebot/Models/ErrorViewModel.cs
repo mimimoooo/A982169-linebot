b7ad6f322e45c4387126996142078cd235c3ca84
ccc <a23523047@gmail.com>
2023-05-09 15:33:18 +0800

    init

---
+++ A982169-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982169_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
